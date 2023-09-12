ac7421ee63e8e817d8efea42c842e02b7ceb2fc3
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-09-12 13:26:20 +0100

    new commit

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ const app = express();
 // Implement CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: `${process.env.CLIENT_URL}`,
     methods: 'GET,POST,PATCH,DELETE',
     credentials: true,
   }),
